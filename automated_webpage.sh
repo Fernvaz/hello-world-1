43ccfd56d9d806555803f7c987e1f35a67ee9ef8
Cloud User <ec2-user@ip-172-31-26-159.us-west-2.compute.internal>
2018-01-26 04:09:55 +0000

    updated the crontab

---
+++ automated_webpage.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo -e "<html> \n<h1> Welcome, NTI-300 </h1> \n
+<h2> Now go away. </h2> \n                                                      
+<h3> It's a secret to everyone. </h3> \n
+<p> The current date is $(date +%c). </p> \n 
+</html>" > /var/www/html/index.html
