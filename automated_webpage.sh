4ecd2bce0cf1b070bae5b56cb1bec2e55f2f1c55
Tedtanium <30739356+Tedtanium@users.noreply.github.com>
2018-01-26 20:38:43 -0800

    Create automated_webpage.sh

---
+++ automated_webpage.sh
@@ -0,0 +1,6 @@
+#!/bin/bash                                                                            #Sheebey-sheebey
+echo -e "<html> \n <h1> Welcome, NTI-300 </h1> \n
+<h2> Now go away. </h2> \n
+<h3> It's a secret to everyone. </h3> \n                                               #Old stuff.
+<p> The current date is $(date +%c) (Updates every five minutes). </p> \n              #Dynamic clock embedded.
+</html>" > /var/www/html/index.html                                          
