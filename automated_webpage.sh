7f487bc6ef0c73b370c00f11a49ac393a33b3dca
root <root@ip-172-31-26-159.us-west-2.compute.internal>
2018-02-02 03:02:19 +0000

    ok

--- automated_webpage.sh
+++ automated_webpage.sh
@@ -2,5 +2,5 @@
 echo -e "<html> \n<h1> Welcome, NTI-300 </h1> \n
 <h2> Now go away. </h2> \n                                                      
 <h3> It's a secret to everyone. </h3> \n
-<p> The current date is $(date +%c). </p> \n 
+<p> The current date is $(date +%c) (Updates every five minutes). </p> \n 
 </html>" > /var/www/html/index.html
