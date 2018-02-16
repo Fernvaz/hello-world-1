0ee33b52774fb81cffc1dae1dca0cd97bd69a606
Tedtanium <30739356+Tedtanium@users.noreply.github.com>
2018-02-16 11:35:43 -0800

    Update forloop.sh

--- forloop.sh
+++ forloop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-for x in $(ls *.conf); do
-cp $x $x.$(date +%F_+%R);
+for x in $(ls *.conf); do                  #Looks for config files at current location.
+cp $x $x.$(date +%F_+%R);                  #Appends date.
 done
