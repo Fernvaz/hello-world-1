5f7d949f31b2709ce17c06a5901ab05f173de3ae
Tedtanium <30739356+Tedtanium@users.noreply.github.com>
2018-02-16 11:39:41 -0800

    Update whileloop.sh

--- whileloop.sh
+++ whileloop.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo -e "$((count - +)) $line"; done < /etc/passwd
+echo -e "$((count - +)) $line"; done < /etc/passwd                              #Appends line numbers to config files.
