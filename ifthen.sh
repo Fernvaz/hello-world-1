60f90fbe3b12c4ada69fac33be856a087171931b
Tedtanium <30739356+Tedtanium@users.noreply.github.com>
2018-02-16 11:37:19 -0800

    Update ifthen.sh

--- ifthen.sh
+++ ifthen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-if [ -e /var/www/html/index.html ]; then
-  exit 0;
+if [ -e /var/www/html/index.html ]; then                      #Checks for the existence of index.html. 
+  exit 0;                                                     #Exits the program "successfully".
 fi
