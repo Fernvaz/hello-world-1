f9010f204e4db86a51df2395eea1ca0c848e1da6
Tedtanium <30739356+Tedtanium@users.noreply.github.com>
2018-02-23 21:10:26 -0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-cat <<ROF > test.sh                                                     #Elects to end the file once "EOF" is typed.
+cat <<EOF > test.sh                                                     #Elects to end the file once "EOF" is typed.
 #!/bin/bash
 echo "This is the best way to echo in a config file, because WYSIWYG"
 EOF
